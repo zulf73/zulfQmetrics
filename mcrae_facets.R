ce798d0079520b213c23b04066a6158645c1bc2e
Zulf Ahmed <zulf@pop-os.localdomain>
2020-10-25 18:05:04 -0500

    Added a simple narrative

--- mcrae_facets.R
+++ mcrae_facets.R
@@ -7,7 +7,7 @@ library(data.table)
 library(dplyr)
 facet_names<-c(
   "Anxiety",
-  "AngryHpstility",
+  "AngryHostility",
   "Depression",
   "Selfconsciousness",
   "Impulsiveness",
@@ -36,6 +36,41 @@ facet_names<-c(
   "AchievementStriving",
   "SelfDiscipline",
   "Deliberation"
+)
+
+facet_state<-list(
+  list("is not anxious", "is anxious"),
+  list( "is not angry and hostile","is angry and hostile"),
+  list( "is not depressed","is depressed"),
+  list( "is not self-consciousness","is self-conscious"),
+  list( "is not impulsive","is impulsive"),
+  list( "is not vulnerable","is vulnerable"),
+  list( "is cold", "is warm"),
+  list( "is quiet", "is talkative"),
+  list( "is not assertive","is assertive"),
+  list( "is passive","is active"),
+  list( "does not seek excitement"," seeks excitement"),
+  list( "is not filled with positive emotions",
+        "is filled with positive emotions"),
+  list( "is not imaginative","is imaginative"),
+  list( "is not beauty oriented", "is beauty oriented"),
+  list( "is not feeling driven","is feeling driven"),
+  list( "does not like to act","likes to act"),
+  list( "is not filled with ideas",
+        "is filled with ideas"),
+  list( "cares for values","does not care for values"),
+  list( "is untrusting","is trusting"),
+  list( "is not straightforward","is straightforward"),
+  list( "is mean","is generous"),
+  list( "is rebellious", "is compliant"),
+  list( "is immodest","is modest"),
+  list( "is tough minded","is tender-minded"),
+  list( "is not competence oriented","is competence oriented"),
+  list( "is disorganised","is organised"),
+  list( "is unreliable", "is reliable"),
+  list( "is a slacker","is achievement striving"),
+  list( "has no self-discipline","has self-discipline"),
+  list( "is unfocused","is deliberate and focused")
 )
 
 facet_data <- c( 
@@ -86,6 +121,60 @@ facet_profile<-function( v, t=1){
   out
 }
 
+center_and_rescale<-function( df ) {
+  dft <- df
+  nc <- ncol(dft)
+  for (k in 2:nc){
+    v <- unlist(df[,k ])
+    v <- (v - min(v))/(max(v)-min(v))
+    meanv <- mean(v)
+    print(paste('meanv',meanv))
+    v <- v - meanv + 0.5
+    dft[,k] <- v
+  }
+  dft
+}
+
+binarise<-function( df ) {
+  dft <- df
+  nc <- ncol(dft)
+  for (k in 2:nc){
+    v <- unlist(df[,k ])
+    v[v>0.5]<-1
+    v[v <= 0.5]<-0
+    dft[,k] <- v
+  }
+  dft
+}
+
+histTotalHigh<-function( facets_types ){
+  totalOneCount <- rep(0, 30)
+  ftmRowSums<-rowSums(ftm_bin[,2:31])
+  for (r in 1:781){
+    v <- ftmRowSums[r]
+    totalOneCount[ v ] <- totalOneCount[v] + 1
+  }
+  totalOneCount
+}
+
+narrative_from_binary<-function(binary_code, name="Tony "){
+  out <- ""
+  n <- length(binary_code)
+  for (j in 1:n){
+    code <- binary_code[j]
+    if (code>1){
+      name <- paste("Type",code,sep="")
+    }
+    if (code==0 || code==1){
+      if (code==1){
+         out <- paste(out, name, facet_state[[j]][[2]],". ",sep="", collapse="")
+      }
+    }
+  }
+  out
+}
+
+
 
 nsample <- 100000
 vf <- matrix(rghyp( nsample, bigfiveDist),ncol=5)
@@ -100,9 +189,14 @@ for (p in 1:nsample){
   nlist <- facet_profile( vf[p,], t=tp)
   proflist[[p]] <- nlist
 }
+
+
+
+#-------- Running code ----
 df <- rbindlist( proflist)
 facet_type_means <- df %>% 
   group_by(type) %>% 
   summarise(across(everything(),mean))
-ggplot( data=facet_type_means, aes(x=TenderMindedness)) 
-+ geom_histogram()
+ftm <- center_and_rescale(facet_type_means)
+#ggplot( data=ftm, aes(x=TenderMindedness)) +geom_histogram()
+bin_ftm<-binarise(ftm)
